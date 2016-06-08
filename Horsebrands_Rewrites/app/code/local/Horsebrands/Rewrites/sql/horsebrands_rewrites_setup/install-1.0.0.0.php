6b8a3560bcad32b78ef441819ddc3d5144a880a1
dennisniederrainer <dennis.niederrainer@gmail.com>
2016-06-08 16:09:10 +0200

    PO - category filter, dbsetup

---
+++ Horsebrands_Rewrites/app/code/local/Horsebrands/Rewrites/sql/horsebrands_rewrites_setup/install-1.0.0.0.php
@@ -0,0 +1,9 @@
+<?php
+$installer = $this;
+$installer->startSetup();
+
+$installer->run("ALTER TABLE purchase_supplier ADD COLUMN sup_logo VARCHAR(255) NULL");
+$installer->run("ALTER TABLE purchase_order ADD COLUMN po_campaign_code VARCHAR(255) NULL");
+$installer->run("ALTER TABLE purchase_order ADD COLUMN po_campaign_week VARCHAR(255) NULL");
+
+$installer->endSetup();
