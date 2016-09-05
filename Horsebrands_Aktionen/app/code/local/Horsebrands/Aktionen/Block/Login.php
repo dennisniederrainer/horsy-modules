d211a4da9cdb07712aa1d245b27ae20197af5af5
dennisniederrainer <dennis.niederrainer@gmail.com>
2016-09-05 15:50:50 +0200

    registration/login form url

--- Horsebrands_Aktionen/app/code/local/Horsebrands/Aktionen/Block/Login.php
+++ Horsebrands_Aktionen/app/code/local/Horsebrands/Aktionen/Block/Login.php
@@ -1,5 +1,14 @@
 <?php
 
 class Horsebrands_Aktionen_Block_Login extends Mage_Core_Block_Template {
-  // todo
+
+  public function getActionUrl($type) {
+    if($type=='registration') {
+      return '#';
+    } elseif($type=='login') {
+      return $this->helper('customer')->getLoginPostUrl();//$this->getUrl('customer/account/loginPost');
+    }
+
+    return '#';
+  }
 }
