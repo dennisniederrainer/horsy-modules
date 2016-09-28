6702c56339c26dcf278e2821ba089c7cc702b1d2
dennisniederrainer <dennis.niederrainer@gmail.com>
2016-09-28 23:49:36 +0200

    topline block

---
+++ Horsebrands_DBSetup/app/code/local/Horsebrands/DBSetup/sql/horsebrands_dbsetup_setup/upgrade-1.0.0.4-1.0.0.5.php
@@ -0,0 +1,25 @@
+<?php
+
+$installer = $this;
+$installer->startSetup();
+
+// footer content
+$block = Mage::getModel('cms/block')->load('topline_content','identifier');
+if($block->getId()){$block->delete();}
+
+$pagecontent = <<<EOF
+<span>Kostenlose Hotline {{config path="general/store_information/phone"}}</span>
+<span>service@horsebrands.de</span>
+<span>14 Tage Rückgaberecht</span>
+<span>Versandkostenfrei ab einem Bestellwert von 30€</span>
+EOF;
+
+$cmsBlock = Mage::getModel('cms/block');
+$cmsBlock->setTitle('Horsebrands Topline');
+$cmsBlock->setIdentifier('topline_content');
+$cmsBlock->setContent($pagecontent);
+$cmsBlock->setIsActive(true);
+$cmsBlock->setStores(array(0));
+$cmsBlock->save();
+
+$installer->endSetup;
